53cdb81964852b3bd750998a0b9558fc1672be86
Rock Hymas <rock@hymasfamily.org>
2018-04-02 16:51:06 -0600

    Revert "Different model for todo text updates"
    This reverts commit 13d2624a48270608d8e29d630d24f8c9ceb45bd0.

--- src/components/SingleTodo.vue
+++ src/components/SingleTodo.vue
@@ -4,7 +4,7 @@
         :class="{completed: todo.completed}">
         <label>
         <input type="checkbox" class="item-checkbox" v-model="completed"/>
-        <input v-model="newTodoText" spellcheck=false class="todo-input" @keyup.enter.prevent="editTodo" @blur.prevent="editTodo"/>
+        <input v-model="todoText" spellcheck=false class="todo-input"/>
         </label>
         <a @click.prevent="deleteTodo(todoId)"
         class="delete pull-right"
@@ -16,12 +16,15 @@
 export default {
   name: 'singletodo',
   props: ['todo', 'todoId'],
-  data () {
-    return {
-      newTodoText: this.todo.text
-    }
-  },
   computed: {
+    todoText: {
+      get: function () {
+        return this.todo.text
+      },
+      set: function (value) {
+        this.$emit('changeTodoText', this.todoId, value)
+      }
+    },
     completed: {
       get: function () {
         return this.todo.completed
@@ -34,10 +37,6 @@ export default {
   methods: {
     deleteTodo (todoId) {
       this.$emit('deleteTodo', todoId)
-    },
-
-    editTodo (e) {
-      this.$emit('changeTodoText', this.todoId, this.newTodoText)
     }
   }
 }
