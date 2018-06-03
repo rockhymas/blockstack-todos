a723a58dd63cc4b13b00454e2942fa321ea774d6
Rock Hymas <rock@hymasfamily.org>
2018-06-03 11:30:57 -0600

    More work on cuedata storage module

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -80,10 +80,6 @@ import SingleTodo from './SingleTodo.vue'
 import draggable from 'vuedraggable'
 import CueData from '../cuedata.js'
 
-var OLD_STORAGE_FILE = 'todolists.json'
-var listsFile = 'lists.json'
-var dataVersionFile = 'version.json'
-
 export default {
   name: 'dashboard',
   components: {
@@ -93,19 +89,19 @@ export default {
   props: ['user'],
   data () {
     return {
-      cuedata: new CueData(window.blockstack, window.automerge),
+      cuedata: new CueData(window.blockstack, window.automerge, window.lodash),
       lists: {},
       loadedList: {},
+      collection: 'active',
       listIndex: 0,
       dataVersion: 0,
-      collection: 'active',
       newListName: '',
       saved: true,
       saving: '',
       focusedId: null,
       pendingFocusId: null,
-      restoreFile: null,
-      throttledPushData: window.lodash.debounce(this.pushDataNow, 3000, { maxWait: 60000 })
+      restoreFile: null
+      // throttledPushData: window.lodash.debounce(this.pushDataNow, 3000, { maxWait: 60000 })
     }
   },
   computed: {
@@ -113,17 +109,17 @@ export default {
       if (typeof this.currentCollection === 'undefined') {
         return
       }
-      return this.lists.lists[this.currentCollection[this.listIndex]]
+      return this.cuedata.lists.lists[this.currentCollection[this.listIndex]]
     },
     currentCollection: function () {
-      if (typeof this.lists.collections === 'undefined') {
+      if (typeof this.cuedata.lists.collections === 'undefined') {
         return
       }
-      return this.lists.collections[this.collection]
+      return this.cuedata.lists.collections[this.collection]
     },
     todoOrder: {
       get: function () {
-        return this.loadedList.todos || []
+        return this.cuedata.loadedList.todos || []
       },
       set: function (value) {
       }
@@ -144,18 +140,18 @@ export default {
     },
     activelists: {
       get: function () {
-        if (typeof this.lists.lists === 'undefined' || typeof this.lists.collections === 'undefined') {
+        if (typeof this.cuedata.lists.lists === 'undefined' || typeof this.cuedata.lists.collections === 'undefined') {
           return []
         }
-        return this.lists.collections['active'].map(l => this.lists.lists[l].name)
+        return this.cuedata.lists.collections['active'].map(l => this.cuedata.lists.lists[l].name)
       }
     },
     archivelists: {
       get: function () {
-        if (typeof this.lists.lists === 'undefined' || typeof this.lists.collections === 'undefined') {
+        if (typeof this.cuedata.lists.lists === 'undefined' || typeof this.cuedata.lists.collections === 'undefined') {
           return []
         }
-        return this.lists.collections['archive'].map(l => this.lists.lists[l].name)
+        return this.cuedata.lists.collections['archive'].map(l => this.cuedata.lists.lists[l].name)
       }
     }
   },
@@ -163,144 +159,15 @@ export default {
     window.addEventListener('beforeunload', this.beforeUnload)
   },
   mounted () {
-    this.fetchData()
     this.cuedata.fetchData()
   },
   methods: {
-    fetchData () {
-      const blockstack = window.blockstack
-
-      blockstack.getFile(dataVersionFile, {decrypt: false})
-      .then((contents) => {
-        if (contents) {
-          this.dataVersion = JSON.parse(contents || 0)
-          console.log(this.dataVersion)
-          if (this.dataVersion < 2) {
-            return this.upgradeData()
-          }
-        } else {
-          return this.initializeData()
-        }
-      })
-      .then(() => {
-        this.fetchVersion2Data()
-      })
-    },
-
-    initializeData () {
-      this.lists = { lists: [] }
-      this.newList()
-      this.throttledPushData()
-      return this.throttledPushData.flush()
-    },
-
-    upgradeData () {
-      if (this.dataVersion === 0) {
-        this.dataVersion = 1
-        return window.blockstack.putFile(dataVersionFile, JSON.stringify(this.dataVersion), {encrypt: false})
-      } else if (this.dataVersion === 1) {
-        this.lists = { lists: [] }
-        return this.fetchVersion1Data()
-          .then(this.upgradeOnev1List, this.initializeData)
-          .then(() => {
-            console.log('setting version to 2')
-            this.dataVersion = 2
-            return window.blockstack.putFile(dataVersionFile, JSON.stringify(this.dataVersion), {encrypt: false})
-          })
-      }
-    },
-
-    upgradeOnev1List (lists) {
-      if (lists.lists.length === 0) {
-        console.log('done upgrading')
-        return Promise.resolve()
-      }
-
-      const listId = Date.now()
-      this.lists.lists.push({ name: lists.lists[0].name, id: listId })
-      this.listIndex = this.lists.lists.length - 1
-      this.loadedList = window.automerge.init()
-      this.loadedList = window.automerge.change(this.loadedList, 'Upgrade v1 list', ll => {
-        ll.name = lists.lists[0].name
-        ll.id = listId
-        ll.todos = lists.lists[0].todos.map((t, i) => { return { id: i, text: t.text, status: t.completed ? 'completed' : 'incomplete' } })
-      })
-
-      this.throttledPushData()
-      return this.throttledPushData.flush()
-      .then(() => {
-        lists = window.automerge.change(lists, 'upgrading', l => l.lists.splice(0, 1))
-        return this.upgradeOnev1List(lists)
-      })
-    },
-
-    pushData () {
-      this.saved = false
-      this.saving = 'Saving...'
-      this.throttledPushData()
-    },
-
-    pushDataNow () {
-      const blockstack = window.blockstack
-      const encrypt = true
-
-      console.log('pushing data: ' + JSON.stringify(this.lists))
-      return blockstack.putFile(listsFile, JSON.stringify(this.lists), encrypt)
-      .then(() => {
-        this.saved = true
-        this.saving = 'Saved'
-        console.log('pushing list')
-        return blockstack.putFile('/lists/' + this.currentList.id + '.json', window.automerge.save(this.loadedList), encrypt)
-      })
-    },
-
-    fetchVersion1Data () {
-      const blockstack = window.blockstack
-      const decrypt = true
-      return blockstack.getFile(OLD_STORAGE_FILE, decrypt)
-      .then((todosText) => {
-        console.log(todosText)
-        var lists = window.automerge.load(todosText) || window.automerge.init()
-        if (typeof lists.lists === 'undefined') {
-          return Promise.reject()
-        }
-        return Promise.resolve(lists)
-      },
-      () => {
-        return Promise.reject()
-      })
-    },
-
-    fetchVersion2Data () {
-      const blockstack = window.blockstack
-      const decrypt = true
-      blockstack.getFile(listsFile, decrypt)
-      .then((listsText) => {
-        console.log(listsText)
-        var lists = JSON.parse(listsText || {})
-        this.listIndex = 0
-        this.lists = lists
-
-        if (typeof this.lists.collections === 'undefined') {
-          this.lists.collections = { active: this.lists.lists.map((l, i) => i), archive: [] }
-        }
-
-        this.newListName = this.currentList.name
-
-        console.log('/lists/' + this.currentList.id + '.json')
-        return blockstack.getFile('/lists/' + this.currentList.id + '.json', decrypt)
-      })
-      .then((contents) => {
-        this.loadedList = window.automerge.load(contents)
-      })
-    },
-
     switchToCollection (collection) {
       if (this.collection === collection) {
         return
       }
 
-      (this.throttledPushData.flush() || Promise.resolve())
+      (this.cuedata.throttledPushData.flush() || Promise.resolve())
       .then(() => {
         this.collection = collection
         this.switchToList(this.currentCollection.length > 0 ? 0 : -1, true)
@@ -312,7 +179,7 @@ export default {
         return
       }
 
-      (this.throttledPushData.flush() || Promise.resolve())
+      (this.cuedata.throttledPushData.flush() || Promise.resolve())
       .then(() => {
         this.listIndex = listIndex
         if (this.listIndex === -1) {
@@ -478,59 +345,10 @@ export default {
     },
 
     backupData () {
-      var listsToBackup = []
-      listsToBackup.push({
-        contents: JSON.parse(JSON.stringify(this.lists)),
-        encrypt: true,
-        path: listsFile,
-        automerge: false
+      this.cuedata.backupData()
+      .then((backup) => {
+        this.download('backup.json', JSON.stringify(backup))
       })
-      listsToBackup.push({
-        path: dataVersionFile,
-        contents: this.dataVersion,
-        encrypt: false,
-        automerge: false
-      })
-      listsToBackup.currentList = 0
-
-      this.backupOneList(listsToBackup)
-      .then((backupLists) => {
-        this.download('backup.json', JSON.stringify(backupLists))
-      })
-    },
-
-    backupOneList (lists) {
-      console.log(lists[0].contents)
-      if (lists.currentList >= lists[0].contents.lists.length) {
-        delete lists.currentList
-        return Promise.resolve(lists)
-      }
-
-      var listPath = '/lists/' + lists[0].contents.lists[lists.currentList].id + '.json'
-      return window.blockstack.getFile(listPath, {decrypt: true})
-      .then((currentList) => {
-        lists.push({
-          contents: this.removeObjectIds(JSON.parse(JSON.stringify(window.automerge.load(currentList)))),
-          encrypt: true,
-          automerge: true,
-          path: listPath
-        })
-        lists.currentList++
-        return this.backupOneList(lists)
-      })
-    },
-
-    removeObjectIds (obj) {
-      console.log(obj)
-      for (var p in obj) {
-        if (p.startsWith('_objectId')) {
-          delete obj[p]
-        } else if (obj.hasOwnProperty(p) && typeof obj[p] === 'object') {
-          this.removeObjectIds(obj[p])
-        }
-      }
-
-      return obj
     },
 
     download (filename, text) {
@@ -547,48 +365,9 @@ export default {
     },
 
     restoreBackup (e) {
-      console.log(e.target.files[0])
-      var file = e.target.files[0]
-
-      return this.readFile(file)
-      .then((contents) => {
-        var lists = JSON.parse(contents)
-        return Promise.all(lists.map((l) => {
-          var contents
-          if (l.automerge) {
-            contents = window.automerge.init()
-            contents = window.automerge.save(window.automerge.change(contents, 'restoring backup', c => {
-              for (var p in l.contents) {
-                if (!p.startsWith('_')) {
-                  c[p] = l.contents[p]
-                }
-              }
-            }))
-          } else {
-            contents = JSON.stringify(l.contents)
-          }
-          console.log(contents)
-          return window.blockstack.putFile(l.path, contents, {encrypt: l.encrypt})
-        }))
-      })
+      this.cuedata.restoreBackup(e.target.files[0])
       .then(() => {
-        // window.location.reload()
-      })
-    },
-
-    readFile (file) {
-      return new Promise((resolve, reject) => {
-        var reader = new FileReader()
-        reader.onload = () => {
-          resolve(reader.result)
-        }
-        reader.onabort = () => {
-          reject(reader.result)
-        }
-        reader.onerror = () => {
-          reject(reader.result)
-        }
-        reader.readAsText(file)
+        window.location.reload()
       })
     }
   }
--- src/cuedata.js
+++ src/cuedata.js
@@ -3,11 +3,12 @@ var listsFile = 'lists.json'
 var dataVersionFile = 'version.json'
 
 export default class {
-  constructor (blockstack, automerge) {
+  constructor (blockstack, automerge, lodash) {
     this.blockstack = blockstack
     this.automerge = automerge
     this.lists = {}
     this.loadedList = {}
+    this.throttledPushData = lodash.debounce(this.pushDataNow, 3000, { maxWait: 60000 })
   }
 
   fetchData () {
@@ -129,6 +130,99 @@ export default class {
     })
     .then((contents) => {
       this.loadedList = this.automerge.load(contents)
+    })
+  }
+
+  backupData () {
+    var listsToBackup = []
+    listsToBackup.push({
+      contents: JSON.parse(JSON.stringify(this.lists)),
+      encrypt: true,
+      path: listsFile,
+      automerge: false
+    })
+    listsToBackup.push({
+      path: dataVersionFile,
+      contents: this.dataVersion,
+      encrypt: false,
+      automerge: false
+    })
+    listsToBackup.currentList = 0
+
+    return this.backupOneList(listsToBackup)
+  }
+
+  backupOneList (lists) {
+    console.log(lists[0].contents)
+    if (lists.currentList >= lists[0].contents.lists.length) {
+      delete lists.currentList
+      return Promise.resolve(lists)
+    }
+
+    var listPath = '/lists/' + lists[0].contents.lists[lists.currentList].id + '.json'
+    return window.blockstack.getFile(listPath, {decrypt: true})
+    .then((currentList) => {
+      lists.push({
+        contents: this.removeObjectIds(JSON.parse(JSON.stringify(window.automerge.load(currentList)))),
+        encrypt: true,
+        automerge: true,
+        path: listPath
+      })
+      lists.currentList++
+      return this.backupOneList(lists)
+    })
+  }
+
+  removeObjectIds (obj) {
+    console.log(obj)
+    for (var p in obj) {
+      if (p.startsWith('_objectId')) {
+        delete obj[p]
+      } else if (obj.hasOwnProperty(p) && typeof obj[p] === 'object') {
+        this.removeObjectIds(obj[p])
+      }
+    }
+
+    return obj
+  }
+
+  restoreBackup (file) {
+    return this.readFile(file)
+    .then((contents) => {
+      var lists = JSON.parse(contents)
+      return Promise.all(lists.map((l) => {
+        var contents
+        if (l.automerge) {
+          contents = window.automerge.init()
+          contents = window.automerge.save(window.automerge.change(contents, 'restoring backup', c => {
+            for (var p in l.contents) {
+              if (!p.startsWith('_')) {
+                c[p] = l.contents[p]
+              }
+            }
+          }))
+        } else {
+          contents = JSON.stringify(l.contents)
+        }
+        console.log(contents)
+        return window.blockstack.putFile(l.path, contents, {encrypt: l.encrypt})
+      }))
+    })
+  }
+
+  readFile (file) {
+    return new Promise((resolve, reject) => {
+      var reader = new FileReader()
+      reader.onload = () => {
+        resolve(reader.result)
+      }
+      reader.onabort = () => {
+        reject(reader.result)
+      }
+      reader.onerror = () => {
+        reject(reader.result)
+      }
+      reader.readAsText(file)
     })
   }
 }
