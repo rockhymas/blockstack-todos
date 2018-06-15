aa4df6cdd3d52a6ded8d663c7fc71b9895c3ae1b
Rock Hymas <rock@hymasfamily.org>
2018-06-15 12:57:29 -0600

    Fleshed out store use for signing in

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <landing v-if="! blockstack.isUserSignedIn()"></landing>
+    <landing v-if="isSignedOut"></landing>
     <dashboard v-if="user" :user="user"></dashboard>
   </div>
 </template>
@@ -14,31 +14,15 @@ import { mapState } from 'vuex'
 export default {
   name: 'app',
   components: {Landing, Dashboard},
-  mounted () {
-    // this.$store.dispatch('signIn')
-    // .catch((pending) => {
-    //   if (pending) {
-    //     window.location = window.location.origin
-    //   }
-    // })
-    if (this.blockstack.isUserSignedIn()) {
-      this.userData = this.blockstack.loadUserData()
-      this.user = new this.blockstack.Person(this.userData.profile)
-      this.user.username = this.userData.username
-    } else if (this.blockstack.isSignInPending()) {
-      this.blockstack.handlePendingSignIn()
-      .then((userData) => {
-        window.location = window.location.origin
-      })
-    }
-  },
-  data () {
-    return {
-      user: null
-    }
+  created () {
+    this.$store.dispatch('signIn')
+    .catch(() => {
+      window.location = window.location.origin
+    })
   },
   computed: mapState([
-    'blockstack'
+    'user',
+    'isSignedOut'
   ])
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,44 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
+var blockstack = require('blockstack')
+console.log(blockstack.isUserSignedIn())
 
 export default new Vuex.Store({
   state () {
     return {
-      blockstack: require('blockstack')
+      blockstack: blockstack,
+      user: null,
+      isSignedOut: !blockstack.isUserSignedIn() && !blockstack.isSignInPending()
+    }
+  },
+  mutations: {
+    signIn (state, user) {
+      state.user = user
+      state.isSignedOut = false
+    }
+  },
+  actions: {
+    signIn ({ commit, state }) {
+      if (state.blockstack.isUserSignedIn()) {
+        console.log('already signed in')
+        var userData = state.blockstack.loadUserData()
+        var user = new state.blockstack.Person(userData.profile)
+        user.username = userData.username
+        commit('signIn', user)
+      } else if (state.blockstack.isSignInPending()) {
+        console.log('pending sign in')
+        return state.blockstack.handlePendingSignIn()
+        .then((userData) => {
+          console.log('pending sign in done')
+          var user = new state.blockstack.Person(userData.profile)
+          user.username = userData.username
+          commit('signIn', user)
+          return Promise.resolve()
+        })
+      }
+
+      return Promise.resolve()
     }
   },
   strict: debug
